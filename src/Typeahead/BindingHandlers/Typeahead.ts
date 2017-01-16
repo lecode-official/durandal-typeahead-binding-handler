07ca862e3002dabecdbae9105ee4d6d22ed1fe23
David Neumann <david.neumann@lecode.de>
2017-01-16 13:53:59 +0100

    Created the basic scaffolding for the binding handler.

---
+++ src/Typeahead/BindingHandlers/Typeahead.ts
@@ -0,0 +1,21 @@
+
+// #region Import Directives
+
+import knockout = require("knockout");
+
+// #endregion
+
+/**
+ * Represents a binding handler for using Twitter typeahead.js and the Bloodhound suggestion engine.
+ */
+knockout.bindingHandlers["typeahead"] = {
+    
+    /**
+     * Initializes the typeahead.js and Bloodhound bindings for the input that this binding is used on.
+     * @param {any} element The DOM element involved in this binding. Should be a text input.
+     * @param {() => any} valueAccessor A JavaScript function that you can call to get the current model property that is involved in this binding.
+     * @param {KnockoutAllBindingsAccessor} allBindingsAccessor A JavaScript object that you can use to access all the model values bound to this DOM element.
+     */
+    init: (element: any, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor) => {
+    }
+};
---
+++ src/Typings/References.d.ts
@@ -0,0 +1,3 @@
+
+/// <reference path="../../bower_components/DefinitelyTyped/jquery/index.d.ts" />
+/// <reference path="../../bower_components/DefinitelyTyped/knockout/index.d.ts" />
