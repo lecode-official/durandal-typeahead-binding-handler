9270b11802b1bba4bc5fab36527bb7a5986e5a7b
David Neumann <david.neumann@lecode.de>
2017-01-16 14:05:44 +0100

    Added typeahead.js and Bloodhound typings to the references.

--- src/Typings/References.d.ts
+++ src/Typings/References.d.ts
@@ -1,3 +1,4 @@
 
 /// <reference path="../../bower_components/DefinitelyTyped/jquery/index.d.ts" />
 /// <reference path="../../bower_components/DefinitelyTyped/knockout/index.d.ts" />
+/// <reference path="../../bower_components/DefinitelyTyped/typeahead/index.d.ts" />
